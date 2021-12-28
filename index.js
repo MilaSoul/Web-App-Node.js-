7e8b341b1c680fd868eb79aa91ac4f03234c59d3
MilaSoul <Mila.dig@yahoo.com>
2021-12-28 21:14:21 +0000

    code to run xml,xsl

--- index.js
+++ index.js
@@ -5,3 +5,37 @@ const   http = require('http'), //This module provides the HTTP server functiona
         xmlParse = require('xslt-processor').xmlParse, //allows to work with XML files
         xsltProcess = require('xslt-processor').xsltProcess, //allows us to uitlise XSL Transformations
         xml2js = require('xml2js'); // XML <-> JSON conversion
+
+        const   router = express(), 
+        server = http.createServer(router);
+
+        router.get('/get/html', function(req, res) {
+
+            res.writeHead(200, {'Content-Type' : 'text/html'});
+        
+            let xml = fs.readFileSync('PaddysCafe.xml', 'utf8'),
+                xsl = fs.readFileSync('PaddysCafe.xsl', 'utf8');
+        
+            console.log(xml);
+            console.log(xsl);
+        
+            let doc = xmlParse(xml),
+                stylesheet = xmlParse(xsl);
+        
+            console.log(doc);
+            console.log(stylesheet);
+        
+            let result = xsltProcess(doc, stylesheet);
+        
+            console.log(result);
+        
+            res.end(result.toString());
+        
+        });
+
+        server.listen(process.env.PORT || 3000, process.env.IP || "0.0.0.0", function() {
+            const addr = server.address();
+            console.log("Server listening at", addr.address + ":" + addr.port)
+        });
+        
+        
