990d18f1764fe578895a906dc6e3948bb6fd8cf1
MilaSoul <Mila.dig@yahoo.com>
2021-12-30 20:36:19 +0000

    backend functions

--- index.js
+++ index.js
@@ -10,8 +10,8 @@ const   http = require('http'), //This module provides the HTTP server functiona
         server = http.createServer(router);
 
         router.use(express.static(path.resolve(__dirname,'views'))); //We serve static content from "views" folder
-        //router.use(express.urlencoded({extended: true})); //We allow the data sent from the client to be encoded in a URL targeting our end point
-        //router.use(express.json()); //We include support for JSON
+        router.use(express.urlencoded({extended: true})); //We allow the data sent from the client to be encoded in a URL targeting our end point
+        router.use(express.json()); //We include support for JSON
 
         router.get('/get/html', function(req, res) {
 
@@ -36,6 +36,51 @@ const   http = require('http'), //This module provides the HTTP server functiona
             res.end(result.toString());
         
         });
+
+        router.post('/post/json', function (req, res) {
+
+            function appendJSON(obj) {
+        
+                console.log(obj)
+        
+                XMLtoJSON('TechShop.xml', function (err, result) {
+                    if (err) throw (err);
+                    
+                    result.catalog.section[obj.sec_n].entry.push({'brand': obj.brand, 'price': obj.price});
+        
+                    console.log(JSON.stringify(result, null, "  "));
+        
+                    JSONtoXML('TechShop.xml', result, function(err){
+                        if (err) console.log(err);
+                    });
+                });
+            };
+        
+            appendJSON(req.body);
+        
+            res.redirect('back');
+        
+        });
+        
+        router.post('/post/delete', function (req, res) {
+        
+            function deleteJSON(obj) {
+        
+                console.log(obj)
+        
+                XMLtoJSON('TechShop.xml', function (err, result) {
+                    if (err) throw (err);
+                    
+                    delete result.catalog.section[obj.section].entry[obj.entree];
+        
+                    console.log(JSON.stringify(result, null, "  "));
+        
+                    JSONtoXML('TechShop.xml', result, function(err){
+                        if (err) console.log(err);
+                    });
+                });
+
+
 
         server.listen(process.env.PORT || 3000, process.env.IP || "0.0.0.0", function() {
             const addr = server.address();
